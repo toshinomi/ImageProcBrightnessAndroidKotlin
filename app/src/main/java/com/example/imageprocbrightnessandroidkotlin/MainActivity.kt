f07ca080bfbf8afe1eb377d7014d738cf732dbfc
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-06-07 17:49:48 +0900

    Update

--- app/src/main/java/com/example/imageprocbrightnessandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/imageprocbrightnessandroidkotlin/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.example.imageprocbrightnessandroidkotlin
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mImageView: ImageView
-    private lateinit var mBitmap: Bitmap
-    private lateinit var mBtnReset: Button
-    private lateinit var mSeekBarAlpha: SeekBar
-    private lateinit var mTextViewAlphaValue: TextView
+    companion object {
+        private lateinit var mBitmap: Bitmap
+        private const val READ_REQUEST_CODE: Int = 42
+        private const val ERROR_MESSAGE_SELECT_IMAGE = "画像選択のエラーが発生しました"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,25 +22,49 @@ class MainActivity : AppCompatActivity() {
         initLayout()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != RESULT_OK) {
+            return
+        }
+
+        when (requestCode) {
+            READ_REQUEST_CODE -> {
+                try {
+                    data?.data?.also { uri ->
+                        val inputStream = contentResolver?.openInputStream(uri)
+                        val image = BitmapFactory.decodeStream(inputStream)
+                        var imageView: ImageView = findViewById(R.id.image)
+                        imageView.setImageBitmap(image)
+                        mBitmap = image
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(this, ERROR_MESSAGE_SELECT_IMAGE, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
     /**
      * レイアウトの初期設定
      */
     private fun initLayout() {
-        mImageView = findViewById(R.id.image)
         mBitmap = BitmapFactory.decodeResource(resources, R.drawable.dog)
 
-        mBtnReset = findViewById(R.id.reset)
-        mBtnReset?.setOnClickListener { onClickBtnReset() }
+        var btnReset: Button = findViewById(R.id.reset)
+        btnReset?.setOnClickListener { onClickBtnReset() }
 
-        mSeekBarAlpha = findViewById(R.id.alpha_seekBar)
-        mSeekBarAlpha?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        var seekBarAlpha: SeekBar = findViewById(R.id.alpha_seekBar)
+        seekBarAlpha?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 var alphaValue = "$p1 %"
-                mTextViewAlphaValue?.text = alphaValue
+                var textViewAlphaValue: TextView = findViewById(R.id.alpha_value)
+                textViewAlphaValue?.text = alphaValue
 
                 var brightness = Brightness()
                 var mutableBitmap = brightness.goImageProcessing(mBitmap, p1)
-                mImageView.setImageBitmap(mutableBitmap.copy(Bitmap.Config.ARGB_8888, false))
+                var imageView: ImageView = findViewById(R.id.image)
+                imageView.setImageBitmap(mutableBitmap.copy(Bitmap.Config.ARGB_8888, false))
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -52,13 +74,26 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        mTextViewAlphaValue = findViewById(R.id.alpha_value)
+        var btnImageSelect: Button = findViewById(R.id.select_image)
+        btnImageSelect?.setOnClickListener { onClickBtnSelectImage() }
     }
 
     /**
      * リセットボタンのクリックイベント
      */
     private fun onClickBtnReset() {
-        mImageView?.setImageBitmap(mBitmap)
+        var imageView: ImageView = findViewById(R.id.image)
+        imageView?.setImageBitmap(mBitmap)
+    }
+
+    /**
+     * 画像選択のクリックイベント
+     */
+    private fun onClickBtnSelectImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, READ_REQUEST_CODE)
     }
 }
