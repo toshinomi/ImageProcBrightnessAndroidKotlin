5a8cc31133d988c5ac2ff6e0bd5ea6ab3aed6a77
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-06-09 22:15:08 +0900

    Update

--- app/src/main/java/com/example/imageprocbrightnessandroidkotlin/Brightness.kt
+++ app/src/main/java/com/example/imageprocbrightnessandroidkotlin/Brightness.kt
@@ -11,9 +11,10 @@ class Brightness {
     /**
      * 明るさの実行
      * @args bitmap ビットマップ
+     * @args alpha  明るさ　%
      * @return return 明るさ後のビットマップ
      */
-    public fun goImageProcessing(bitmap: Bitmap, alpha: Int): Bitmap {
+    public fun goImageProcessing(bitmap: Bitmap, alphaValue: Int): Bitmap {
         var pixels = IntArray(bitmap.width * bitmap.height);
         var resultPixels = IntArray(bitmap.width * bitmap.height)
 
@@ -23,7 +24,7 @@ class Brightness {
             var red = android.graphics.Color.red(argb)
             var green = android.graphics.Color.green(argb)
             var blue = android.graphics.Color.blue(argb)
-            var alpha = android.graphics.Color.alpha(argb) * alpha / 100
+            var alpha = android.graphics.Color.alpha(argb) * alphaValue / 100
             resultPixels[count] = android.graphics.Color.argb(alpha, red, green, blue)
         }
 
